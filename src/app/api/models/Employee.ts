1509608518be2808ce5b00eab7ab3b9143157193
GFrancelino <gustavo.francelino@gmail.com>
2020-12-02 08:21:53 +0000

    fix bug where where it was impossible to create a new employee

--- src/app/api/models/Employee.ts
+++ src/app/api/models/Employee.ts
@@ -5,36 +5,37 @@ import {environment} from '../../../environments/environment';
 import {PermissionModels} from '@api/permissions';
 
 export class Employee extends Person {
-  id = 0;
-  private = false;
-  firstName = '';
-  lastName = '';
-  name = '';
-  email = '';
-  phone = '';
-  schedule = 0;
-  instagram = '';
-  photoUrl = '';
-  bio = '';
+  id;
+  private;
+  firstName;
+  lastName;
+  name;
+  email;
+  phone;
+  schedule;
+  instagram;
+  photoUrl;
+  bio;
   services: number[] = [];
 
   static fromJs(data: any): Employee {
     data = typeof data === 'object' ? data : {};
     const result = new Employee();
     if (data) {
-      result.id = data.id;
-      result.private = data.private;
-      result.firstName = data.firstName;
-      result.lastName = data.lastName;
-      result.name = data.name;
-      result.email = data.email;
-      result.phone = data.phone;
-      result.schedule = data.schedule;
-      result.instagram = data.instagram;
+      // this.project = _data["project"] ? ProjectBindingModel.fromJS(_data["project"]) : <any>null;
+      result.id = data.id ? data.id : 0 ;
+      result.private = !!data.private;
+      result.firstName = data.firstName ? data.firstName : '';
+      result.lastName = data.lastName ? data.lastName : '';
+      result.name = data.name ? data.name : '';
+      result.email = data.email ? data.email : '';
+      result.phone = data.phone ? data.phone : '';
+      result.schedule = data.schedule ? data.schedule : 0;
+      result.instagram = data.instagram ? data.instagram : '';
       result.photoUrl = data.profileImg ? environment.imageStorage + data.profileImg
         : environment.assetUrl + 'img/default-avatar.jpg';
-      result.bio = data.bio;
-      result.services = data.services;
+      result.bio = data.bio ? data.bio : '';
+      result.services = data.services ? data.services : [];
     }
     return result;
   }
