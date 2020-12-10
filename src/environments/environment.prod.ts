20851ee9bb0426b1ab9cc55637b67fda88a8532c
GFrancelino <gustavo.francelino@gmail.com>
2020-12-10 21:24:08 +0000

    update env url to a definite url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: window.location.origin + '/api/',
+  apiUrl: 'https://api.kalendario.ie/api/',
   assetUrl: 'static/ang/assets/',
   imageStorage: 'https://res.cloudinary.com/gchahm/',
   stripePk: 'pk_test_51HG6eSAe2CG5hLhgSySK6m0KWXT7MgcoZdCAtqfFyg8nqwL9ZklNpCVke2H1EOX6uLI0jqYuRF08wb4X65gfDE1V00inHpeisT',
